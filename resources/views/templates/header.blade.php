70bbf0eb6c3f256706f135d33722511096da502b
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-02-25 21:13:45 -0300

    inicial

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -16,8 +16,7 @@
 
                 <!-- User Account: style can be found in dropdown.less -->
                 <li class="dropdown user user-menu">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <!--img src="{{url('dist/img/user2-160x160.jpg')}}" class="user-image" alt="User Image"-->
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
                         <span class="hidden-xs"> {{session('users')}}  </span>
                     </a>
                     <ul class="dropdown-menu">             
