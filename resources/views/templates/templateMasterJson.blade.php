7efa6e4aea52ed5c2a127d1350493c1e4c5927ad
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-02-25 21:22:13 -0300

    inicial

--- resources/views/templates/templateMasterJson.blade.php
+++ resources/views/templates/templateMasterJson.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=2' name='viewport'>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
